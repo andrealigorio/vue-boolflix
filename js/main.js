39865939cff489af32f0b29cbeea3fea0e73e9fd
Andrea Ligorio <andrea85kro@gmail.com>
2021-04-06 16:15:05 +0200

    Milestone 2 terminata

--- js/main.js
+++ js/main.js
@@ -6,21 +6,43 @@ var app = new Vue({
         language: "it-IT",
         searchText: "",
         movies: [],
+        tv: [],
         search: false
     },
     methods: {
         searchMovie() {
             if(this.searchText != "") {
                 axios
-                .get("https://api.themoviedb.org/3/search/movie?api_key=" + this.apiKey + "&language=" + this.language + "&query=" + this.searchText)
-                .then(response => {
-                    this.movies = response.data.results;
-                    this.search = true;
+                    .get("https://api.themoviedb.org/3/search/movie?api_key=" + this.apiKey + "&language=" + this.language + "&query=" + this.searchText)
+                    .then(response => {
+                        this.movies = response.data.results;
+                        this.search = true;
                 });
+                axios
+                    .get("https://api.themoviedb.org/3/search/tv?api_key=" + this.apiKey + "&language=" + this.language + "&query=" + this.searchText)
+                    .then(response => {
+                        this.tv = response.data.results;
+                        this.search = true;
+                    });
             } else {
                 this.movies = [];
+                this.tv = [];
             }
             this.search = false;
+        },
+        printFlag(array) {
+            let languageLink = "https://lipis.github.io/flag-icon-css/flags/4x3/"
+            if(array.original_language != "en") {  //Perchè lingua EN equivale alla bandiera della GB
+                languageLink = languageLink + array.original_language + ".svg";
+            } else {
+                languageLink = languageLink + "gb.svg";
+            }
+            return languageLink;
+        },
+        printStars(number) {
+            let vote = Math.ceil(number/2);
+            var fullStars = vote;
+            return fullStars;
         }
     }
 });
